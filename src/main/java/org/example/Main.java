bdf0b9db73f0cb0e8c36009f1d7e28b6c46174e4
Скрипник Анна Юріївна <anastasia.shevchenko.2.7.0.2@gmail.com>
2025-09-29 18:35:11 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,33 @@ package org.example;
 
 import patient.model.Patient;
 
+/** Головний клас програми
+ * Створює масив пацієнтів і виводить їх за різними критеріями.
+ * */
+
 public class Main {
     public static void main(String[] args) {
 
-        Patient[] patientsArray = {
+        /** Основний метод програми
+         *
+         * @param args не використовується
+         */
+
+        Patient[] patientsArray = createPatientsArray();
+
+        printPatientsByDiagnose(patientsArray, "schizophrenia");
+        printPatientsByMedicalCardNum(patientsArray, 100, 160);
+        printPatientsByFirstFigureOfPhoneNum(patientsArray, "9");
+
+    }
+
+    /**
+     * Створює та повертає масив пацієнтів.
+     *
+     * @return масив об'єктів класу Patient
+     */
+    private static Patient[] createPatientsArray() {
+        return new Patient[]{
                 new Patient(1,"Sliusar","Viktoria","Volodymurivna", "Lutsk","99-510-3626",101,"allergy"),
                 new Patient(2,"Skrypnyk","Anna","Yuriyivna","Vinnytsia","69-936-8340",120,"schizophrenia"),
                 new Patient(3,"Babiy","Olha","Andriyivna","Dubno","95-338-1594",123,"schizophrenia"),
@@ -15,14 +38,14 @@ public class Main {
                 new Patient(7,"Pasichuk","Alla","Valeriyivna","Polonne","66-101-3426",170,"flu"),
                 new Patient(8,"Melnyk","Oleksandr","Dmitrovich","Odessa","96-123-1651",200,"astma"),
         };
-
-        printPatientsByDiagnose(patientsArray, "schizophrenia");
-        printPatientsByMedicalCardNum(patientsArray, 100, 160);
-        printPatientsByFirstFigureOfPhoneNum(patientsArray, "9");
-
     }
 
-
+    /**
+     * Виводить у консоль пацієнтів із заданим діагнозом.
+     *
+     * @param patients масив пацієнтів
+     * @param diagnose діагноз для пошуку
+     */
     private static void printPatientsByDiagnose(Patient[] patients, String diagnose){
         System.out.println("\nPatients with diagnose: " + diagnose);
 
@@ -33,7 +56,14 @@ public class Main {
         }
     }
 
-
+    /**
+     * Виводить у консоль пацієнтів, номери медичних карток яких
+     * знаходяться в межах вказаного діапазону.
+     *
+     * @param patients масив пацієнтів
+     * @param startOfRange початок діапазону
+     * @param endOfRange кінець діапазону
+     */
     private static void printPatientsByMedicalCardNum(Patient[] patients, int startOfRange, int endOfRange){
         System.out.println("\nPatients from this range (from " + startOfRange + " to " + endOfRange + ") of medical card values: ");
 
@@ -44,7 +74,13 @@ public class Main {
         }
     }
 
-
+    /**
+     * Виводить у консоль пацієнтів, номери телефонів яких
+     * починаються з вказаної цифри, та підраховує їх кількість.
+     *
+     * @param patients масив пацієнтів
+     * @param firstFigure перша цифра телефонного номера
+     */
     private static void printPatientsByFirstFigureOfPhoneNum(Patient[] patients, String firstFigure){
         System.out.println("\nPatients whose phone number starts with figure " + firstFigure + ":");
         int counter = 0;
