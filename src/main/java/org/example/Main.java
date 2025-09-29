159f8e9cc3986caafa752a5922504370088212f9
Raayalee <skrupnukua@gmail.com>
2025-09-29 18:16:12 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,61 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import patient.model.Patient;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Patient[] patientsArray = {
+                new Patient(1,"Sliusar","Viktoria","Volodymurivna", "Lutsk","99-510-3626",101,"allergy"),
+                new Patient(2,"Skrypnyk","Anna","Yuriyivna","Vinnytsia","69-936-8340",120,"schizophrenia"),
+                new Patient(3,"Babiy","Olha","Andriyivna","Dubno","95-338-1594",123,"schizophrenia"),
+                new Patient(4,"Basarab","Yaroslav","Olehovych","Lviv","76-512-3020",142,"flu"),
+                new Patient(5,"Dyachenko","Nazaretto","Yaneznayu","Kiyv","69-906-8890",180,"allergy"),
+                new Patient(6,"Kucheryava","Anna","Dmitryvna","Medenichy","90-300-1555",158,"schizophrenia"),
+                new Patient(7,"Pasichuk","Alla","Valeriyivna","Polonne","66-101-3426",170,"flu"),
+                new Patient(8,"Melnyk","Oleksandr","Dmitrovich","Odessa","96-123-1651",200,"astma"),
+        };
+
+        printPatientsByDiagnose(patientsArray, "schizophrenia");
+        printPatientsByMedicalCardNum(patientsArray, 100, 160);
+        printPatientsByFirstFigureOfPhoneNum(patientsArray, "9");
+
+    }
+
+
+    private static void printPatientsByDiagnose(Patient[] patients, String diagnose){
+        System.out.println("\nPatients with diagnose: " + diagnose);
+
+        for (Patient el: patients){
+            if(el.getDiagnose().equals(diagnose)){
+                System.out.println(el);
+            }
         }
     }
+
+
+    private static void printPatientsByMedicalCardNum(Patient[] patients, int startOfRange, int endOfRange){
+        System.out.println("\nPatients from this range (from " + startOfRange + " to " + endOfRange + ") of medical card values: ");
+
+        for (Patient el: patients) {
+            if(el.getMedicalCardNum() >= startOfRange && el.getMedicalCardNum() <= endOfRange){
+                System.out.println(el);
+            }
+        }
+    }
+
+
+    private static void printPatientsByFirstFigureOfPhoneNum(Patient[] patients, String firstFigure){
+        System.out.println("\nPatients whose phone number starts with figure " + firstFigure + ":");
+        int counter = 0;
+
+        for (Patient el: patients){
+            if(el.getPhoneNum().startsWith(firstFigure)){
+                System.out.println(el);
+                counter++;
+            }
+        }
+        System.out.println("Number of patients with this first figure in phone number: " + counter);
+    }
+
 }
--- src/main/java/patient/model/Patient.java
+++ src/main/java/patient/model/Patient.java
@@ -1,4 +1,192 @@
 package patient.model;
 
+/**
+ * Модель сутності {@code Patient}, що представляє пацієнта лікарні
+ *
+ * Клас зберігає базову інформацію про пацієнта, а саме:
+ * ідентифікатор, ПІБ, адресу, номер телефону,
+ * номер медичної карти, а також діагноз
+ *
+ * У програмі створений клас допомагає зчитувати та подавати інформацію
+ * про пацієнтів, фільтрувати її у залежності від вимог завдання
+ */
 public class Patient {
+
+    /** Унікальний ідентифікатор пацієнта */
+    private int patientID;
+
+    /** Прізвище пацієнта */
+    private String lastName;
+
+    /** Ім'я пацієнта */
+    private String firstName;
+
+    /** По батькові пацієнта */
+    private String paternal;
+
+    /** Адреса пацієнта */
+    private String address;
+
+    /** Номер телефону пацієнта */
+    private String phoneNum;
+
+    /** Номер медичної карти пацієнта */
+    private int medicalCardNum;
+
+    /** Діагноз пацієнта */
+    private String diagnose;
+
+
+    /** Повний конструктор класу Patient, що створює об'єкт
+     *
+     * @param patientID ID пацієнта
+     * @param lastName прізвище пацієнта
+     * @param firstName ім'я пацієнта
+     * @param paternal по батькові пацієнта
+     * @param address адреса пацієнта
+     * @param phoneNum номер телефону пацієнта
+     * @param medicalCardNum номер медичної карти пацієнта
+     * @param diagnose діагноз пацієнта
+     */
+    public Patient (int patientID, String lastName, String firstName, String paternal,
+                    String address, String phoneNum, int medicalCardNum, String diagnose){
+        this.patientID = patientID;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.paternal = paternal;
+        this.address = address;
+        this.phoneNum = phoneNum;
+        this.medicalCardNum = medicalCardNum;
+        this.diagnose = diagnose;
+    }
+
+
+    /** Гетери для повернення даних:
+     * @return ID пацієнта
+     */
+    public int getPatientID() {
+        return patientID;
+    }
+
+    /**
+     * @return прізвище пацієнта
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @return ім'я пацієнта
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @return по батькові пацієнта
+     */
+    public String getPaternal() {
+        return paternal;
+    }
+
+    /**
+     * @return адреса пацієнта
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @return номер телефону пацієнта
+     */
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    /**
+     * @return номер медичної карти пацієнта
+     */
+    public int getMedicalCardNum() {
+        return medicalCardNum;
+    }
+
+    /**
+     * @return діагноз пацієнта
+     */
+    public String getDiagnose() {
+        return diagnose;
+    }
+
+    /** Сетери для оновлення даних:
+     * @param patientID новий ID пацієнта
+     */
+    public void setPatientID(int patientID) {
+        this.patientID = patientID;
+    }
+
+    /**
+     * @param lastName нове прізвище пацієнта
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @param firstName нове ім'я пацієнта
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @param paternal нове по батькові пацієнта
+     */
+    public void setPaternal(String paternal) {
+        this.paternal = paternal;
+    }
+
+    /**
+     * @param address нова адреса пацієнта
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @param phoneNum новий номер телефону пацієнта
+     */
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    /**
+     * @param medicalCardNum новий номер медичної картки
+     */
+    public void setMedicalCardNum(int medicalCardNum) {
+        this.medicalCardNum = medicalCardNum;
+    }
+
+    /**
+     * @param diagnose новий діагноз
+     */
+    public void setDiagnose(String diagnose) {
+        this.diagnose = diagnose;
+    }
+
+    /**Метод для повернення інформації про пацієнта у зручному форматі на консоль
+     *
+     * @return рядок із даними пацієнта
+     */
+
+    @Override
+    public String toString() {
+        return "\nPatient info:\n" + "id = " + getPatientID() +
+                "\nlast name = " + getLastName() +
+                "\nfirst name = " + getFirstName() +
+                "\npaternal = " + getPaternal() +
+                "\naddress = " + getAddress() +
+                "\nphone number = " + getPhoneNum() +
+                "\nmedical card number = " + getMedicalCardNum() +
+                "\ndiagnose = " + getDiagnose();
+    }
 }
